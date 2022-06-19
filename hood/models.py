658e5ce56782f552635239ac84f89148f525d58e
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-19 14:06:20 +0300

    add model class for profile

--- hood/models.py
+++ hood/models.py
@@ -28,30 +28,28 @@ class Neighbour(models.Model):
         hood_name = self.hood_name
         self.hood_name = hood_name
 
-
-
-# #class profile
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     idNo = models.IntegerField(default=0)
-#     email = models.CharField(max_length=30, blank=True)
-#     profile_pic = CloudinaryField('profile')
-#     bio = models.TextField(max_length=500, blank=True)
-#     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, blank=True, null=True)
+#class profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    idNo = models.IntegerField(default=0)
+    email = models.CharField(max_length=30, blank=True)
+    #profile_pic = CloudinaryField('profile')
+    bio = models.TextField(max_length=500, blank=True)
+    neighbour = models.ForeignKey(Neighbour, on_delete=models.CASCADE, blank=True, null=True)
     
-#     def __str__(self):
-#         return self.user.username
-#     @receiver(post_save, sender=User)
-#     def update_user_profile(sender, instance, created, **kwargs):
-#         if created:
-#             Profile.objects.create(user=instance)
-#         instance.profile.save()
-#     def save_profile(self):
-#         self.save()
-#     def delete_profile(self):
-#         self.delete()
-#     def update_profile(cls, id):
-#         Profile.objects.get(user_id=id)
+    def __str__(self):
+        return self.user.username
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
+    def update_profile(cls, id):
+        Profile.objects.get(user_id=id)
 
 
 # #class Bussiness
