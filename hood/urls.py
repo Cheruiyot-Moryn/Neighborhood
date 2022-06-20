947f8fb24d265b094f2e2d5bc41adfa4bc2520ef
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-20 16:38:00 +0300

    create paths for the views

---
+++ hood/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import include, url
+from django.urls import path 
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+urlpatterns=[
+    url(r'^$',views.index,name='index'),
+    url('register/',views.registration, name='registration'),
+    url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    url('new-hood/', views.add_neighbourhood, name='newhood'),
+    url('profile/', views.profile, name='profile'),
+    url('single_hood/<hood_id>', views.single_neighbourhood, name='single-hood'),
+    path('join_hood/<id>', views.join_neighbourhood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_neighbourhood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_neighbourhood, name='single-hood'),
+    path('<hood_id>/post/', views.create_post, name='post'),
+    path('search/', views.search_business, name='search'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
