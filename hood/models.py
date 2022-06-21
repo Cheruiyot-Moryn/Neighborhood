8c85d87780f4d19891b7c6a73e3d4c1aef8de614
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-21 10:00:58 +0300

    add background image

--- hood/models.py
+++ hood/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import  User
-#from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -10,7 +10,7 @@ class Neighbour(models.Model):
     hood_name = models.CharField(max_length=200)
     hood_location = models.CharField(max_length=200)
     hood_description = models.TextField(max_length=500, blank=True)
-    #hood_photo = CloudinaryField('photo', default='photo')
+    hood_photo = CloudinaryField('photo', default='photo')
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
 
 
