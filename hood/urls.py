dc128f360a091d0fe637edef991fdd9c6ded9dd2
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-22 17:10:13 +0300

    add styling

--- hood/urls.py
+++ hood/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import include, url
-from django.urls import path 
+from django.conf.urls import  url
+from django.urls import path ,include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
