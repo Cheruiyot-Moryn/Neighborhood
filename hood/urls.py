596ac37956457a8c95575bf46b58dc43b018a352
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-22 17:08:05 +0300

    add styling

--- hood/urls.py
+++ hood/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns=[
-    url(r'^$',views.index,name='index'),
+    path('',views.index,name='index'),
     url('register/',views.registration, name='registration'),
     url('login/',auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
